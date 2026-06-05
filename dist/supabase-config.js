e4de2989f1ad6d85d7aee9a2acfe63e3caf852bc
bryandy-svg <bryan.dy@lmsfm.com>
2026-06-05 17:56:40 +1000

    Delete dist directory

--- dist/supabase-config.js
+++
@@ -1,4 +0,0 @@
-// Vercel replaces this file during build using that project's environment variables.
-// Do not hardcode production or test keys here, otherwise both apps can point to the same database.
-export const SUPABASE_URL = "";
-export const SUPABASE_ANON_KEY = "";
