7f1e2f011063aee4e5634a9ba3e0016209ada206
bryandy-svg <bryan.dy@lmsfm.com>
2026-08-10 10:04:11 +1000

    Delete supabase directory

--- supabase/migrations/20260729_trucking_tipping_charge.sql
+++
@@ -1,4 +0,0 @@
-alter table public.trucking_moves
-  add column if not exists tipping_charge numeric not null default 0;
-
-grant select, insert, update on public.trucking_moves to anon, authenticated;
