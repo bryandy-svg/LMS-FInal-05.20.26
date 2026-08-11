7e7fc1f8649f93a5dffd3482b75ed05f6fe57876
bryandy-svg <bryan.dy@lmsfm.com>
2026-08-11 10:09:34 +1000

    Delete supabase directory

--- supabase/migrations/20260729_roll_off_tipping_fee_scope.sql
+++
@@ -1,8 +0,0 @@
-update public.trucking_rates
-set parent_service = 'Roll Off Service'
-where lower(trim(coalesce(category, ''))) = 'tipping fee';
-
-update public.trucking_requests
-set tipping_fee = null
-where lower(trim(coalesce(service_needed, ''))) <> 'roll off service'
-  and tipping_fee is not null;
